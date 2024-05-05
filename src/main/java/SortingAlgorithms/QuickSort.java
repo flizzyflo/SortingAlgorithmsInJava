00b42e088b2bd3692bfbc4797dddc8e8d20e8ff8
flizzyflo <vip3r1@gmx.de>
2024-05-05 12:41:42 +0200

    added docstring to methods and class

--- src/main/java/SortingAlgorithms/QuickSort.java
+++ src/main/java/SortingAlgorithms/QuickSort.java
@@ -5,13 +5,28 @@ import main.java.UserInterface.DrawPanel;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class implements the Quick Sort algorithm to sort a list of elements.
+ * It extends the BaseSearch class to inherit common sorting functionality.
+ *
+ * @param <E> The type of elements to be sorted, which must extend Number and implement Comparable.
+ */
 public class QuickSort<E extends Number & Comparable<E>> extends BaseSearch<E> {
 
-
+    /**
+     * Constructor to initialize QuickSort with data to be sorted.
+     *
+     * @param dataToSort The data to be sorted.
+     */
     public QuickSort (List<E> dataToSort) {
         this.dataToSort = dataToSort;
     }
 
+    /**
+     * Constructor to initialize QuickSort with a DrawPanel for visualization.
+     *
+     * @param drawPanel The DrawPanel for visualization.
+     */
     public QuickSort (DrawPanel drawPanel) {
         this.drawPanel = drawPanel;
         this.listeners = new ArrayList<>();
@@ -35,6 +50,12 @@ public class QuickSort<E extends Number & Comparable<E>> extends BaseSearch<E> {
         this.quickSort(left, right);
     }
 
+    /**
+     * Recursively sorts the input list using the Quick Sort algorithm.
+     *
+     * @param left The left index of the sublist to be sorted.
+     * @param right The right index of the sublist to be sorted.
+     */
     private void quickSort(int left, int right) {
 
         if (left >= right) {
