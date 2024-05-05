3b6c22c88478cf614697b8167ca2c4380fca13c1
flizzyflo <vip3r1@gmx.de>
2024-05-05 12:41:28 +0200

    added docstring to methods and class

--- src/main/java/SortingAlgorithms/BubbleSort.java
+++ src/main/java/SortingAlgorithms/BubbleSort.java
@@ -6,12 +6,25 @@ import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class implements the Bubble Sort algorithm to sort a list of elements.
+ * It extends the BaseSearch class to inherit common sorting functionality.
+ *
+ * @param <E> The type of elements to be sorted, which must extend Number and implement Comparable.
+ */
 public class BubbleSort<E extends Number & Comparable<E>> extends BaseSearch<E> {
 
     private boolean countSteps;
     private boolean showComparedValues;
     private DrawPanel drawPanel = null;
 
+    /**
+     * Constructor to initialize BubbleSort with data to be sorted, and options for counting steps and displaying compared values.
+     *
+     * @param data The data to be sorted.
+     * @param countSteps Flag to indicate whether to count sorting steps.
+     * @param showComparedValues Flag to indicate whether to display compared values during sorting.
+     */
     public BubbleSort(final List<E> data, boolean countSteps, boolean showComparedValues) {
         this.dataToSort = data;
         this.countSteps = countSteps;
@@ -19,6 +32,11 @@ public class BubbleSort<E extends Number & Comparable<E>> extends BaseSearch<E> 
         this.listeners = new ArrayList<>();
     };
 
+    /**
+     * Constructor to initialize BubbleSort with a DrawPanel for visualization.
+     *
+     * @param drawPanel The DrawPanel for visualization.
+     */
     public BubbleSort(DrawPanel drawPanel) {
         this.drawPanel = drawPanel;
         this.listeners = new ArrayList<>();
@@ -34,6 +52,9 @@ public class BubbleSort<E extends Number & Comparable<E>> extends BaseSearch<E> 
         super.notifyListeners();
     }
 
+    /**
+     * Implements the Bubble Sort algorithm to sort the data.
+     */
     @Override
     public void sort() {
 
@@ -81,6 +102,13 @@ public class BubbleSort<E extends Number & Comparable<E>> extends BaseSearch<E> 
         }
     }
 
+    /**
+     * Checks if the left value is greater than the right value.
+     *
+     * @param leftValue The left value.
+     * @param rightValue The right value.
+     * @return True if the left value is strictly greater than the right value, false otherwise.
+     */
     private boolean leftValueBiggerThanRight(E leftValue, E rightValue) {
         return leftValue.compareTo(rightValue) > 0;
     }
