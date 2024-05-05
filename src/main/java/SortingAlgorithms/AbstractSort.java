b4fac7f0891e6cab2bebe257bca97a12588f5e29
flizzyflo <vip3r1@gmx.de>
2024-05-05 12:41:22 +0200

    added docstring to methods and class

--- src/main/java/SortingAlgorithms/AbstractSort.java
+++ src/main/java/SortingAlgorithms/AbstractSort.java
@@ -3,6 +3,9 @@ package main.java.SortingAlgorithms;
 
 public abstract class AbstractSort<E extends Number & Comparable<E>> {
 
+    /**
+     * Sort method to be implemented by concrete sorting algorithms.
+     */
     abstract public void sort();
 
 }
