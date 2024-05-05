c494682a61d792be85bfd513dcd894ace809ebe5
flizzyflo <vip3r1@gmx.de>
2024-05-05 12:41:48 +0200

    added docstring to methods and class

--- src/main/java/SortingAlgorithms/SelectionSort.java
+++ src/main/java/SortingAlgorithms/SelectionSort.java
@@ -5,17 +5,35 @@ import main.java.UserInterface.DrawPanel;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class implements the Selection Sort algorithm to sort a list of elements.
+ * It extends the BaseSearch class to inherit common sorting functionality.
+ *
+ * @param <E> The type of elements to be sorted, which must extend Number and implement Comparable.
+ */
 public class SelectionSort<E extends Number & Comparable<E>> extends BaseSearch<E> {
 
     private boolean countSteps = false;
     private boolean showComparedValues = false;
 
+    /**
+     * Constructor to initialize SelectionSort with data to be sorted and options for step counting and showing compared values.
+     *
+     * @param data The data to be sorted.
+     * @param countSteps Flag to indicate whether to count steps during sorting.
+     * @param showComparedValues Flag to indicate whether to show compared values during sorting.
+     */
     public SelectionSort(final List<E> data, boolean countSteps, boolean showComparedValues) {
         this.dataToSort = data;
         this.countSteps = countSteps;
         this.showComparedValues = showComparedValues;
     }
 
+    /**
+     * Constructor to initialize SelectionSort with a DrawPanel for visualization.
+     *
+     * @param drawPanel The DrawPanel for visualization.
+     */
     public SelectionSort(DrawPanel drawPanel) {
         this.drawPanel = drawPanel;
         this.listeners = new ArrayList<>();
@@ -73,7 +91,13 @@ public class SelectionSort<E extends Number & Comparable<E>> extends BaseSearch<
         }
     }
 
-
+    /**
+     * Checks if the current value is lower than the minimum value.
+     *
+     * @param currentValue The index of the current value being compared.
+     * @param minValueIdx The index of the minimum value found so far.
+     * @return True if the current value is lower than the minimum value, otherwise false.
+     */
     private boolean currentValueIsLowestValue(int currentValue, int minValueIdx) {
         return this.dataToSort.get(currentValue).compareTo(this.dataToSort.get(minValueIdx)) < 0;
     }
